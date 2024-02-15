550a9e8a8254b6f6aacbfefddd2695d1cf66fefd
kiran <kiransaidu179@gmail.com>
2024-02-15 14:39:36 +0530

    First Commit

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Adding");
+int num1 = 1;
+int num2 = 3;
+Console.WriteLine("{0} + {1} = {2}", num1, num2, (num1 + num2));
